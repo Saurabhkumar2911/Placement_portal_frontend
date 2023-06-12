05b80a79d8c1aebd7ee10561e86e052a9fb4112f
Saurabh kumar <saurabhkr2911@gmail.com>
2023-06-12 22:42:25 +0530

    update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,31 +4,31 @@ function Footer() {
   return (
     <footer className="footer-distributed">
       <div className="footer-right">
-        <a href="#">
+        <a href="https://github.com/Saurabhkumar2911">
           <i className="fa fa-facebook"></i>
         </a>
-        <a href="#">
+        <a href="https://github.com/Saurabhkumar2911">
           <i className="fa fa-twitter"></i>
         </a>
-        <a href="#">
+        <a href="https://github.com/Saurabhkumar2911">
           <i className="fa fa-linkedin"></i>
         </a>
-        <a href="#">
+        <a href="https://github.com/Saurabhkumar2911">
           <i className="fa fa-github"></i>
         </a>
       </div>
 
       <div className="footer-left">
         <p className="footer-links">
-          <a className="link-1" href="#">
+          <a className="link-1" href="https://github.com/Saurabhkumar2911">
             Home
           </a>
 
-          <a href="#">Pricing</a>
+          <a href="https://github.com/Saurabhkumar2911">Pricing</a>
 
-          <a href="#">About</a>
+          <a href="https://github.com/Saurabhkumar2911">About</a>
 
-          <a href="#">Contact</a>
+          <a href="https://github.com/Saurabhkumar2911">Contact</a>
         </p>
 
         <p>GreyView &copy; 2023</p>
